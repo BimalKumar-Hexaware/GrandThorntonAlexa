2590f39c05c61119e806d50eda3f3cb6a61c13a2
Bimal Kumar <40321@hexaware.com>
2018-10-01 11:30:58 +0530

    ff

--- handlers.js
+++ handlers.js
@@ -1,6 +1,11 @@
 var alexa = require("alexa-app");
 var alexaApp = new alexa.app("test");
 
+oppStatusIntent = false;
+oppFilterIntent = false;
+oppStatus = "";
+oppFilter = "";
+
 alexaApp.launch(function (req, res) {
     res.say("You launched the app!").shouldEndSession(false);
 });
@@ -10,8 +15,22 @@ alexaApp.intent("testIntent", function (req, res) {
 });
 
 alexaApp.intent("oppStatusIntent", function (req, res) {
+    oppStatusIntent = true;
     console.log("Slots", req.data.request.intent.slots);
-    res.say("Success!").shouldEndSession(false);
+    oppStatus = req.data.request.intent.slots.status.value;
+
+    res.say("You can filter the opportunities by date and estimated revenue. Which one you want?").shouldEndSession(false);
+});
+
+alexaApp.intent("oppFilterIntent", function (req, res) {
+    oppFilterIntent = true;
+    console.log("Slots", req.data.request.intent.slots);
+    oppFilter = req.data.request.intent.slots.filter.value;
+    if (oppFilter == "revenue") {
+        res.say("Please mention the range").shouldEndSession(false);
+    } else if (oppFilter == "date") {
+        res.say("Please mention the date").shouldEndSession(false);
+    }
 });
 
 module.exports = alexaApp;
